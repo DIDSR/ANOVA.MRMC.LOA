33b25acecde39e329379cf331f354bb2b26edef5
SiWen314 <si.wen@fda.hhs.gov>
2020-12-11 20:21:25 -0500

    Initial Commit

---
+++ R/validateMRMCVarEstimation.R
@@ -0,0 +1,113 @@
+#' Validate and characterize the MRMC limits of agreement estimates
+#'
+#' @description
+#' This function simulates MRMC study with \code{nReader} readers and \code{nCase} cases. From the output of \code{laWRBM.anova}
+#' and \code{laBRBM.anova}, we exact the variance estimations for WRBM and BRBM differences. It outputs the Monte Carlo
+#' mean of the variance estimation across all the simulation trials. The relative bias of the Monte Carlo mean comparing
+#' to the theoretical value of the variance and the coefficient of variation is also calculated.
+#'
+#' @details
+#' Let \eqn{\hat{V}_{BR}^{12}} denote the estimation of the variance for the BRBM difference for each simulated MRMC study, then
+#' the relative bias is defined as \eqn{relativeBias~(\hat{V}_{BR}^{12})=(\sum{\hat{V}_{BR}^{12}}/{nTrials}-V_{BR}^{12})/V_{BR}^{12}},
+#' and the coefficient of variantion is defined as \eqn{CV~(\hat{V}_{BR}^{12})=sd(\hat{V}_{BR}^{12})/{V_{BR}^{12}}}, where \eqn{sd()}
+#' denote sample standard deviation.
+#'
+#' @param nR.list
+#' A \eqn{r}-dimension array for the number of readers in each MRMC study
+#' @param nC.list
+#' A \eqn{c}-dimension array for the number of cases in each MRMC study
+#' @param alpha_R.list
+#' A \eqn{a}-dimension array for the reader related parameter
+#' @param sigma_C.list
+#' A \eqn{b}-dimension array for the case related parameter
+#' @param nTrials
+#' Number of MRMC simulations. Default is 1000
+#'
+#' @return
+#' A dataframe with \eqn{r*c*a*b} rows. Each column is as following:
+#' \describe{
+#'   \item{nReader}{Number of Readers}
+#'   \item{nCase}{Number of Cases}
+#'   \item{alpha_R}{Reader related parameter}
+#'   \item{sigma_C}{Case related parameter}
+#'   \item{WR_var_MCmean}{Monte Carlo mean of variance estimation for the WRBM difference score}
+#'   \item{WR_var_MCvar}{Monte Carlo variance of variance estimation for the WRBM difference score}
+#'   \item{True_WR_var}{Theoretical value for WRBM variance derived from the simulation model}
+#'   \item{WR_var_relative_bias}{relative bias for WRBM variance}
+#'   \item{WR_CV}{coefficient of variation for WRBM variance}
+#'   \item{BR_var_MCmean}{Monte Carlo mean of variance estimation for the BRBM difference score}
+#'   \item{BR_var_MCvar}{Monte Carlo variance of variance estimation for the BRBM difference score}
+#'   \item{True_BR_var}{Theoretical value for BRBM variance derived from the simulation model}
+#'   \item{BR_var_relative_bias}{relative bias for BRBM variance}
+#'   \item{BR_CV}{coefficient of variation for BRBM variance}
+#'   }
+#' @export
+#'
+#' @examples
+#' library(iMRMC)
+#' nR.list <- c(3,4,5)
+#' nC.list <- c(100)
+#' sigma_C.list <- c(1)
+#' alpha_R.list <- c(10)
+#' result <- validateMRMCVarEstimation(alpha_R.list, sigma_C.list)
+#'
+validateMRMCVarEstimation <- function(nR.list, nC.list, alpha_R.list, sigma_C.list, nTrials = 1000){
+
+  agg_result <- NULL
+  # RNG ----
+  RNGkind("L'Ecuyer-CMRG")
+  set.seed(1)
+
+  for (nR in nR.list){
+    for (nC in nC.list){
+      for(alpha_R in alpha_R.list){
+        for(sigma_C in sigma_C.list){
+          # Config ----
+
+          config = sim.new.Hierarchical.config(nR=nR, nC=nC, sigma_C = sigma_C,
+                                               alpha_R = alpha_R, C_dist = 'normal',
+                                               modalityID = c("testA","testB"))
+
+          configs.Task = rep(list(config),nTrials)
+
+          # Simulation & LOA ----
+
+          dFrames = lapply(configs.Task, FUN = sim.new.Hierarchical)
+          Results.WR = lapply(dFrames, FUN = laWRBM.anova)
+          Results.BR = lapply(dFrames, FUN = laBRBM.anova)
+
+          # Aggregate ----
+
+          WR_result <- do.call("rbind", Results.WR)
+          BR_result <- do.call("rbind", Results.BR)
+
+          WR_var_MCmean <- mean(WR_result$var.1obs)
+          WR_var_MCvar <- var(WR_result$var.1obs)
+
+          BR_var_MCmean <- mean(BR_result$var.1obs)
+          BR_var_MCvar <- var(BR_result$var.1obs)
+
+          # Theoretical value ----
+
+          True_WR_var <- 2*sigma_C^2 + 2/(alpha_R-1)
+          True_BR_var <- 2*sigma_C^2 + 4/(alpha_R-1)
+
+
+          result<-data.frame(nReader = nR, nCase = nC, alpha_R = alpha_R, sigma_C = sigma_C,
+                             WR_var_MCmean = WR_var_MCmean,WR_var_MCvar = WR_var_MCvar,
+                             True_WR_var = True_WR_var,
+                             WR_var_relative_bias = (WR_var_MCmean - True_WR_var)/True_WR_var,
+                             WR_CV <- sqrt(WR_var_MCvar)/True_WR_var,
+                             BR_var_MCmean = BR_var_MCmean,BR_var_MCvar = BR_var_MCvar,
+                             True_BR_var = True_BR_var,
+                             BR_var_relative_bias = (BR_var_MCmean - True_BR_var)/True_BR_var,
+                             BR_CV <- sqrt(BR_var_MCvar)/True_BR_var)
+          agg_result <- rbind(agg_result, result)
+        }
+      }
+    }
+  }
+
+  return(agg_result)
+
+}
---
+++ R/validateSimulation.R
@@ -0,0 +1,102 @@
+#' Verify the simulation is consistent with the derived theoretical values
+#'
+#' @description
+#' This function simulates independent WRBM and BRBM difference scores and outputs the Monte Carlo mean and variance
+#' across the trials for each parameter setting. The relative bias of the Monte Carlo variance comparing to the
+#' theoretical value of the variance is also calculated.
+#'
+#' @details
+#' Let \eqn{\tilde{V}_{BR}^{12}} denote the Monte Carlo variance of the BRBM difference, then the relative bias is defined
+#' as \eqn{relativeBias~(\tilde{V}_{BR}^{12})=(\tilde{V}_{BR}^{12}-V_{BR}^{12})/V_{BR}^{12}}.
+#'
+#' @param alpha_R.list
+#' A \eqn{a}-dimension array for the reader related parameter
+#' @param sigma_C.list
+#' A \eqn{b}-dimension array for the case related parameter
+#' @param nTrials
+#' Number of independent trials. Default is 100000
+#'
+#' @return
+#' A dataframe with \eqn{a*b} rows. Each column is as following:
+#' \describe{
+#'   \item{sigma_C}{Case related parameter}
+#'   \item{alpha_R}{Reader related parameter}
+#'   \item{WR_mean}{Monte Carlo mean of the WRBM difference score}
+#'   \item{WR_var}{Monte Carlo variance of the WRBM difference score}
+#'   \item{BR_mean}{Monte Carlo mean of the BRBM difference score}
+#'   \item{BR_var}{Monte Carlo variance of the BRBM difference score}
+#'   \item{True_WR_var}{Theoretical value for WRBM variance derived from the simulation model}
+#'   \item{True_BR_var}{Theoretical value for BRBM variance derived from the simulation model}
+#'   \item{WR_var_relative_bias}{relative bias for WRBM variance}
+#'   \item{BR_var_relative_bias}{relative bias for BRBM variance}
+#' }
+#' @export
+#'
+#' @examples
+#' library(iMRMC)
+#' sigma_C.list <- c(1)
+#' alpha_R.list <- c(10)
+#' result <- validateSimulation(alpha_R.list, sigma_C.list)
+#'
+validateSimulation <- function(alpha_R.list, sigma_C.list, nTrials = 100000){
+  # For the independent simulation, we simulate 2 readers and 1 case
+  nR = 2
+  nC = 1
+
+  result = NULL
+  # RNG ----
+  RNGkind("L'Ecuyer-CMRG")
+  set.seed(1)
+
+  for(sigma_C in sigma_C.list){
+    for(alpha_R in alpha_R.list){
+
+      # Config ----
+      config = sim.new.Hierarchical.config(nR=nR, nC=nC, sigma_C = sigma_C,
+                                           alpha_R = alpha_R, C_dist = 'normal',
+                                           modalityID = c("testA","testB"))
+
+      configs.Task = rep(list(config),nTrials)
+
+      # Simulation ----
+
+      dFrames = lapply(configs.Task, FUN = sim.new.Hierarchical)
+
+      # Calculate independent WRBR and BRBR independence ----
+      Results.WR = lapply(dFrames, FUN = function(x){
+        WR_diff <- x[x$readerID == 'reader1' & x$caseID == 'Case1' & x$modalityID == 'testA','score']-
+          x[x$readerID == 'reader1' & x$caseID == 'Case1' & x$modalityID == 'testB','score']
+        return(WR_diff)
+      })
+      Results.BR = lapply(dFrames, FUN = function(x){
+        BR_diff <- x[x$readerID == 'reader1' & x$caseID == 'Case1' & x$modalityID == 'testA','score']-
+          x[x$readerID == 'reader2' & x$caseID == 'Case1' & x$modalityID == 'testB','score']
+        return(BR_diff)
+      })
+
+      # Aggregate ----
+
+      WR_mean <- mean(array(unlist(Results.WR)))
+      WR_var <- var(array(unlist(Results.WR)))
+
+      BR_mean <- mean(array(unlist(Results.BR)))
+      BR_var <- var(array(unlist(Results.BR)))
+
+      # Theoretical value ----
+
+      True_WR_var <- 2*sigma_C^2 + 2/(alpha_R-1)
+      True_BR_var <- 2*sigma_C^2 + 4/(alpha_R-1)
+
+      # Relative bias ----
+
+      WR_var_relative_bias <- (WR_var - True_WR_var)/True_WR_var
+      BR_var_relative_bias <- (BR_var - True_BR_var)/True_BR_var
+
+      result <- rbind(result, data.frame(sigma_C=sigma_C, alpha_R=alpha_R, WR_mean = WR_mean, WR_var = WR_var,
+                                         BR_mean = BR_mean, BR_var = BR_var, True_WR_var = True_WR_var,
+                                         True_BR_var=True_BR_var, WR_var_relative_bias=WR_var_relative_bias,
+                                         BR_var_relative_bias = BR_var_relative_bias))
+    }
+  }
+  return(result)
+}
